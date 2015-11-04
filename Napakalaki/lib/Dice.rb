5c90d3480a1c6d2e030a9f6c1dd5339682949cba
adrian <adrian@AMGmint>
2015-11-04 13:25:48 +0100

    Añadiendo clases P2 (muchos fallos aun...)

--- Napakalaki/lib/Dice.rb
+++ Napakalaki/lib/Dice.rb
@@ -1,11 +1,17 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+require 'singleton'
 module NapakalakiGame
-
 class Dice
-  
-  
+  include Singleton
+  @@instance=nil
+  def self.getInstance
+    if(@@instance==nil)
+      @@instance=Dice.new
+    end
+    return @@instance 
+  end
   def nextNumber()
     return 1+rand(6)
   end
