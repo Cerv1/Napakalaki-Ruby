2e12ed47335079abb6c88c10a93b54e19013e26c
12adrianmorente <adry_12_mg@hotmail.com>
2016-01-13 10:48:52 +0100

    P4

--- Napakalaki/lib/SpecificBadConsequence.rb
+++ Napakalaki/lib/SpecificBadConsequence.rb
@@ -29,20 +29,7 @@ module NapakalakiGame
     end
     
     def adjustToFitTreasureLists(v,h)         
-#      vKind=v.collect{|t| t.type}
-#      hKind=h.collect{|t| t.type}
-#          
-#      [TreasureKind::HELMET, TreasureKind::ARMOR, TreasureKind::ONEHAND, TreasureKind::BOTHHANDS].each do |tkind|
-#        if(@specificVisibleTreasures != nil)
-#          adVisList=adVisList+[tkind]*[vKind.select{|t| t==tkind}.size, @specificVisibleTreasures.select{|tt| tt==tkind}.size].min
-#        end
-#        if(@specificHiddenTreasures != nil)
-#          adHidList=adHidList+[tkind]*[hKind.select{|t| t==tkind}.size, @specificHiddenTreasures.select{|tt| tt==tkind}.size].min
-#        end
-#      end
-#      mal_rollo = SpecificBadConsequence.new(@text,0,adVisList, adHidList)
-#      return mal_rollo
-#    end
+
         if(@specificHiddenTreasures != nil)
             adHidList = Array.new
         else
@@ -73,7 +60,7 @@ module NapakalakiGame
         end
         end
         
-        adjustedbc = SpecificBadConsequence.new(@text, 0, adVisList, adHidLst)
+        adjustedbc = SpecificBadConsequence.new(@text, 0, adVisList, adHidList)
         return adjustedbc
   end
   
